9e08deff2298b3e3b001ad4723da44e1b6b0c58c
Yonatan Csasznik <yonacs@gmail.com>
2025-05-27 15:45:16 +0300

    Add loggers for structured logging and update dependencies
    Implemented loggers using log4js for handling request, stack, and independent logs with specific configurations. Updated `package.json` and `package-lock.json` to include `log4js` and its dependencies. Modified `.gitignore` to handle log-related artifacts and added `logs/.gitkeep` to retain the logs directory.

---
+++ loggers/index.js
@@ -0,0 +1,76 @@
+/**
+ * @module loggers
+ * @description Initializes and exports all loggers using log4js
+ * @requires log4js
+ */
+
+const loggers = require('log4js');
+const fs = require('fs');
+const path = require('path');
+
+// Ensure the logs directory exists
+const logsDir = path.join(__dirname, '../logs');
+if (!fs.existsSync(logsDir)) {
+    fs.mkdirSync(logsDir);
+}
+
+// Define '{date-time} {log-level}: {log-message} | request #{request-number}' as the standard log format
+const layout = {
+    type: 'pattern',
+    pattern: '%d{dd-MM-yyyy HH:mm:ss.SSS} %p: %m'
+}
+
+// Configure log4js with multiple appenders and logger categories
+loggers.configure({
+    appenders: {
+        request: { type: 'file', filename: 'logs/requests.log', layout: layout },
+        stack: { type: 'file', filename: 'logs/stack.log', layout: layout},
+        independent: { type: 'file', filename: 'logs/independent.log', layout: layout },
+        console: { type: 'console' }
+    },
+    categories: {
+        default: { appenders: ['console'], level: 'info' },
+
+        // In charge of logging each incoming request of any type to the server
+        'request-logger': {
+            appenders: ['request', 'console'],
+            level: 'debug'
+        },
+        // In charge of logging information on all the stack behavior
+        'stack-logger': {
+            appenders: ['stack'],
+            level: 'info'
+        },
+        // In charge of logging information on all the independent behavior
+        'independent-logger': {
+            appenders: ['independent'],
+            level: 'info'
+        }
+    }
+});
+
+// Export initialized loggers
+
+/**
+ * @constant {Logger} requestLogger
+ * @description In charge of logging each incoming request of any type to the server
+ */
+const requestLogger = loggers.getLogger('request-logger');
+
+/**
+ * @constant {Logger} stackLogger
+ * @description In charge of logging information on all the stack behavior
+ */
+const stackLogger = loggers.getLogger('stack-logger');
+
+/**
+ * @constant {Logger} independentLogger
+ * @description In charge of logging information on all the independent behavior
+ */
+const independentLogger = loggers.getLogger('independent-logger');
+
+module.exports = {
+    requestLogger,
+    stackLogger,
+    independentLogger
+};
